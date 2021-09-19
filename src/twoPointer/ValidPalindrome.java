f2db1ee635b7645da7e08457313e16e9080ead4c
linxiaopeng <17352714151@163.com>
2021-09-19 23:45:09 +0800

    feat 双指针 删除一个字符是否回文

---
+++ src/twoPointer/ValidPalindrome.java
@@ -0,0 +1,29 @@
+package twoPointer;
+
+public class ValidPalindrome {
+    public boolean validPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 给定字符串，从i到j位置是否为回文
+     *
+     * @param s
+     * @param i
+     * @param j
+     * @return
+     */
+    public boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
