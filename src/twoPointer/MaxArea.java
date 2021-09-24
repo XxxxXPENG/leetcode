b445883a2145a2b655e39999045577a148a716eb
linxiaopeng <17352714151@163.com>
2021-09-24 23:12:52 +0800

    feat 双指针 容器最大容积

---
+++ src/twoPointer/MaxArea.java
@@ -0,0 +1,37 @@
+package twoPointer;
+
+/**
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，
+ * 容器能够容纳水（表示为蓝色部分）的最大值为 49。
+ *
+ */
+public class MaxArea {
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[]{4,3,2,1,4}));
+    }
+
+
+    public static int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+        while (left < right) {
+            int leftHeight = height[left];
+            int rightHeight = height[right];
+            int width = right - left;
+            int temp = Math.min(leftHeight, rightHeight) * width;
+            maxArea = Math.max(maxArea, temp);
+            //选择较短边，向内移动
+            if (leftHeight < rightHeight) {
+                left++;
+            }else{
+                right--;
+            }
+        }
+
+        return maxArea;
+
+
+    }
+}
