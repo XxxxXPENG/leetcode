2c630dc0c6e5bec7322d65f40ac9f91e58281c03
linxiaopeng <17352714151@163.com>
2021-09-24 23:41:25 +0800

    feat 双指针 删除一个字符是否回文 （代码优化）

--- src/twoPointer/MaxArea.java
+++ src/twoPointer/MaxArea.java
@@ -5,13 +5,22 @@ package twoPointer;
  * 输出：49
  * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，
  * 容器能够容纳水（表示为蓝色部分）的最大值为 49。
- *
  */
 public class MaxArea {
     public static void main(String[] args) {
-        System.out.println(maxArea(new int[]{4,3,2,1,4}));
+        System.out.println(maxArea(new int[]{4, 3, 2, 1, 4}));
     }
 
+
+    public int maxArea2(int[] height) {
+        int left = 0, right = height.length - 1, res = 0;
+        while (left < right) {
+            res = height[left] > height[right] ?
+                    Math.max(res, (right - left) * height[left++])
+                    : Math.max(res, (right - left) * height[right--]);
+        }
+        return res;
+    }
 
     public static int maxArea(int[] height) {
         int left = 0, right = height.length - 1;
@@ -25,7 +34,7 @@ public class MaxArea {
             //选择较短边，向内移动
             if (leftHeight < rightHeight) {
                 left++;
-            }else{
+            } else {
                 right--;
             }
         }
