1d831d3a78a3e34ad8e8eef420e4b882e2be39b7
linxiaopeng <17352714151@163.com>
2021-09-25 22:15:37 +0800

    feat 双指针 接雨水

---
+++ src/twoPointer/Trap.java
@@ -0,0 +1,45 @@
+package twoPointer;
+
+public class Trap {
+    public static void main(String[] args) {
+        System.out.println(trap2(new int[]{4,2,0,3,2,5}));
+    }
+
+    public static int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int res = 0;
+        while (left < right) {
+            leftMax = Math.max(height[left], leftMax);
+            rightMax = Math.max(height[right], rightMax);
+            if (height[left] < height[right]) {
+                res += leftMax - height[left];
+                ++left;
+            } else {
+                res += rightMax - height[right];
+                --right;
+            }
+        }
+        return res;
+    }
+
+    public static int trap2(int[] height) {
+        int ans = 0;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if (height[left] < height[right]) {
+                ans += leftMax - height[left];
+                ++left;
+            } else {
+                ans += rightMax - height[right];
+                --right;
+            }
+        }
+        return ans;
+    }
+
+
+}
