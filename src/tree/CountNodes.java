35d1db5ee865b6cd63fdb05461335aafce08c264
linxiaopeng <17352714151@163.com>
2021-10-26 23:39:26 +0800

    feat-力扣222 树 完全二叉树的节点个数

---
+++ src/tree/CountNodes.java
@@ -0,0 +1,15 @@
+package tree;
+
+public class CountNodes {
+    public int countNodes(TreeNode root) {
+        if (root != null) {
+            return 0;
+        }
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return left + right + 1;
+    }
+
+
+
+}
