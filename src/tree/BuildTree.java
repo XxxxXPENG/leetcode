83e8945b048142a69ad34332bb4ac528494f5ce2
linxiaopeng <17352714151@163.com>
2021-11-05 23:52:55 +0800

    feat 力扣105 通过先序、中序遍历 复原二叉树

--- src/tree/BuildTree.java
+++ src/tree/BuildTree.java
@@ -1,18 +1,36 @@
 package tree;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * todo
  * 根据前序遍历和中序遍历，恢复二叉树
  */
 public class BuildTree {
-//    public TreeNode buildTree(int[] preorder, int[] inorder) {
-//
-//
-//        //
-//    }
-//
-//
-//    TreeNode build(int[] preorder,int[] inorder, int l, int r) {
-//
-//    }
+    Map<Integer, Integer> map = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = inorder.length;
+        for (int i = 0; i < n; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return build(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+
+
+    TreeNode build(int[] preorder, int[] inorder, int preLeft, int preRight, int inLeft, int inRight) {
+        if (preLeft > preRight) {
+            return null;
+        }
+        int preRoot = preLeft;
+        TreeNode root = new TreeNode(preorder[preRoot]);
+        int inRoot = map.get(preorder[preRoot]);
+        int leftSubTreeSize = inRoot - inLeft;
+
+        root.left = build(preorder, inorder, preLeft + 1, preLeft + leftSubTreeSize, inLeft, inRoot - 1);
+        root.right = build(preorder, inorder, preLeft + leftSubTreeSize + 1, preRight, inRoot + 1, inRight);
+        return root;
+    }
 }
