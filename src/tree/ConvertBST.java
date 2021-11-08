68897396927b382222beb6a3fe486369533ffbdd
linxiaopeng <17352714151@163.com>
2021-11-08 22:39:57 +0800

    feat 力扣1038 二叉搜索树变为累加树

---
+++ src/tree/ConvertBST.java
@@ -0,0 +1,17 @@
+package tree;
+
+public class ConvertBST {
+    //反中序遍历 （右 中 左）
+    public TreeNode convertBST(TreeNode root) {
+        dfs(root, 0);
+        return root;
+    }
+
+    public int dfs(TreeNode root, int val) {
+        if (root == null) {
+            return val;
+        }
+        root.val += dfs(root.right, val);
+        return dfs(root.left, root.val);
+    }
+}
