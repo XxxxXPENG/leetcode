4837161b648375b98a42091be8933dce95994961
linxiaopeng <17352714151@163.com>
2021-11-26 23:25:11 +0800

    力扣114

--- src/tree/Flatten.java
+++ src/tree/Flatten.java
@@ -1,6 +1,31 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Flatten {
+
+    public void flatten2(TreeNode root) {
+        List<TreeNode> list = new ArrayList<>();
+        preOrderTraversal(root, list);
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode pre = list.get(i - 1);
+            TreeNode cur = list.get(i);
+            pre.left = null;
+            pre.right = cur;
+        }
+    }
+
+    private void preOrderTraversal(TreeNode root, List<TreeNode> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root);
+        preOrderTraversal(root.left, list);
+        preOrderTraversal(root.right, list);
+    }
+
     /**
      * 将二叉树转为链表
      * @param root
