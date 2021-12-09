47516260020cee6eb906151145dd5efcc5176052
linxiaopeng <17352714151@163.com>
2021-12-09 23:18:57 +0800

    全排列Ⅱ  47 组合总和Ⅱ 40

---
+++ src/tu/CombinationSum2.java
@@ -0,0 +1,41 @@
+package tu;
+
+import java.util.*;
+
+public class CombinationSum2 {
+
+    public static void main(String[] args) {
+        CombinationSum2 c = new CombinationSum2();
+        System.out.println(c.combinationSum2(new int[]{10, 1, 2, 7, 6, 1, 5}, 8));
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<Integer> path = new ArrayDeque<>();
+        Arrays.sort(candidates);
+        dfs(res, candidates, path, 0, target);
+        return res;
+    }
+
+    private void dfs(List<List<Integer>> res, int[] candidates, Deque<Integer> path, int begin, int target) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = begin; i < candidates.length; i++) {
+            if (target - candidates[i] < 0) {
+                return;
+            }
+
+            if (i > begin && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            path.add(candidates[i]);
+            dfs(res, candidates, path, i + 1, target - candidates[i]);
+            path.removeLast();
+        }
+    }
+}
---
+++ src/tu/PermuteUnique.java
@@ -0,0 +1,39 @@
+package tu;
+
+import java.util.*;
+
+public class PermuteUnique {
+    public static void main(String[] args) {
+        PermuteUnique p = new PermuteUnique();
+        System.out.println(p.permuteUnique(new int[]{1,1,2}));
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<Integer> path = new ArrayDeque<>();
+        boolean[] vis = new boolean[nums.length];
+        dfs(res, path, nums,vis);
+        return res;
+    }
+
+    private void dfs(List<List<Integer>> res, Deque<Integer> path, int[] nums, boolean[] vis) {
+        if (path.size() == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            //重复元素不能在同一层
+            if (i > 0 && nums[i - 1] == nums[i] && !vis[i-1]) {
+                continue;
+            }
+            if (!vis[i]) {
+                vis[i] = true;
+                path.add(nums[i]);
+                dfs(res, path, nums, vis);
+                path.removeLast();
+                vis[i] = false;
+            }
+        }
+    }
+}
