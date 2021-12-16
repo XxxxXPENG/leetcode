c2055ef443b472ae7d41371ed5b580b669371341
linxiaopeng <17352714151@163.com>
2021-12-16 23:14:39 +0800

    5. 最长回文子串

---
+++ src/str/LongestPalindrome.java
@@ -0,0 +1,36 @@
+package str;
+
+public class LongestPalindrome {
+    public static void main(String[] args) {
+        LongestPalindrome s = new LongestPalindrome();
+        System.out.println(s.expandAroundCenter("abacc", 1, 1));
+    }
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        //因为跳出循环时，2边都会多走一次
+        return right - left - 1;
+    }
+
+
+}
