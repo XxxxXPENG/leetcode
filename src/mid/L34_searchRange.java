788edcd9ab6b9d38a9c9e263af0c21d2d16b6d07
linxiaopeng <17352714151@163.com>
2022-01-13 23:13:48 +0800

    34. logN方式

--- src/mid/L34_searchRange.java
+++ src/mid/L34_searchRange.java
@@ -5,9 +5,35 @@ import java.util.Arrays;
 public class L34_searchRange {
     public static void main(String[] args) {
         L34_searchRange a = new L34_searchRange();
-        System.out.println(Arrays.toString(a.searchRange(new int[]{2,2}, 2)));
+        System.out.println(Arrays.toString(a.searchRange2(new int[]{1}, 1)));
     }
 
+    public int[] searchRange2(int[] nums, int target) {
+
+        int left = getIndex(nums, target, true);
+        int right = getIndex(nums, target, false)-1;
+        if (left <= right && nums[left] == target && nums[right] == target) {
+            return new int[]{left, right};
+        }
+        return new int[]{-1, -1};
+    }
+
+
+    public int getIndex(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1,ans = nums.length;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if ((lower && nums[mid] >= target) || nums[mid] > target) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    //时间复杂度O(N)
     public int[] searchRange(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         int[] res = new int[2];
