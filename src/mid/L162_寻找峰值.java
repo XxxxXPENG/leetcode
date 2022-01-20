83dbf0b3dec1154eaa7319a318a5a5ec445ece20
linxiaopeng <17352714151@163.com>
2022-01-20 23:10:49 +0800

    162. 寻找峰值

---
+++ src/mid/L162_寻找峰值.java
@@ -0,0 +1,16 @@
+package mid;
+
+public class L162_寻找峰值 {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
