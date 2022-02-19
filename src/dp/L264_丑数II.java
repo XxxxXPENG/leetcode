712c99eb8b9949fac0cbc70847b223680885f87f
linxiaopeng <17352714151@163.com>
2022-02-19 23:58:36 +0800

    dp day10

---
+++ src/dp/L264_丑数II.java
@@ -0,0 +1,21 @@
+package dp;
+
+public class L264_丑数II {
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        int p2 = 1, p3 = 1, p5 = 1;
+        for (int i = 2; i <= n; i++) {
+            int num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
+            dp[i] = Math.min(Math.min(num2, num3), num5);
+            if (num2 == dp[i]) {
+                p2++;
+            } else if (num3 == dp[i]) {
+                p3++;
+            } else if(num5 == dp[i]){
+                p5++;
+            }
+        }
+        return dp[n];
+    }
+}
--- src/dp/L91_解码方法.java
+++ src/dp/L91_解码方法.java
@@ -1,7 +1,50 @@
 package dp;
 
 public class L91_解码方法 {
-//    public int numDecodings(String s) {
-//
-//    }
+
+    public static void main(String[] args) {
+        L91_解码方法 a = new L91_解码方法();
+        System.out.println(a.numDecodings2("226"));
+    }
+
+
+    public int numDecodings(String s) {
+        int len = s.length();
+        //让下标从1开始
+        s = " " + s;
+        char[] arr = s.toCharArray();
+        int[] dp = new int[len + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= len; i++) {
+            int a = arr[i] - '0', b = (arr[i - 1] - '0') * 10 + a;
+            if (a > 0 && a <= 9) {
+                dp[i] = dp[i - 1];
+            }
+            if (b > 9 && b <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[len];
+    }
+
+    //常量空间
+    public int numDecodings2(String s) {
+        int[] dp = new int[3];
+        dp[0] = 1;
+        int len = s.length();
+        s = " " + s;
+        char[] arr = s.toCharArray();
+        for (int i = 1; i <= len; i++) {
+            dp[i % 3] = 0;
+            int a = arr[i] - '0', b = (arr[i - 1] - '0') * 10 + a;
+            if (a > 0 && a <= 9) {
+                dp[i % 3] = dp[(i - 1) % 3];
+            }
+            if (b > 9 && b <= 26) {
+                dp[i % 3] += dp[(i - 2) % 3];
+            }
+        }
+        return dp[len % 3];
+    }
+
 }
