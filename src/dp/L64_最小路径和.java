25bc4418136c1cb04a58a0cc6280190040a3ed14
linxiaopeng <17352714151@163.com>
2022-02-23 22:50:23 +0800

    dp day13

---
+++ src/dp/L64_最小路径和.java
@@ -0,0 +1,34 @@
+package dp;
+
+public class L64_最小路径和 {
+
+    public static void main(String[] args) {
+        L64_最小路径和 a = new L64_最小路径和();
+        int[][] grid = {
+                {1, 3, 1},
+                {1, 5, 1},
+                {4, 2, 1}
+        };
+
+        System.out.println(a.minPathSum(grid));
+    }
+
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                } else if (i == 0){
+                    grid[i][j] += grid[0][j - 1];
+                } else if (j == 0) {
+                    grid[i][j] += grid[i - 1][0];
+                } else {
+                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                }
+
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}
