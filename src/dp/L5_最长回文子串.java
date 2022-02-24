4b7245d32cbbedeb32879d6d27f2f95c79a5f933
linxiaopeng <17352714151@163.com>
2022-02-24 22:53:22 +0800

    dp day14

---
+++ src/dp/L5_最长回文子串.java
@@ -0,0 +1,28 @@
+package dp;
+
+public class L5_最长回文子串 {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int oddLen = expand(s, i, i);
+            int evenLen = expand(s, i, i + 1);
+            int len = Math.max(oddLen, evenLen);
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
