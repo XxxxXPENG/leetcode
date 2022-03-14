1715afa43eb41a4533db53a3fbe5ff1b88b7a809
linxiaopeng <17352714151@163.com>
2022-03-14 22:49:19 +0800

    hot day9

---
+++ src/hot100/L32_最长有效括号.java
@@ -0,0 +1,25 @@
+package hot100;
+
+import java.util.Stack;
+
+public class L32_最长有效括号 {
+    public int longestValidParentheses(String s) {
+        int res = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    res = Math.max(res, i - stack.peek());
+                }
+            }
+        }
+        return res;
+
+    }
+}
