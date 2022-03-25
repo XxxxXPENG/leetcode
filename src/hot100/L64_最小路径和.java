de2910f1195e8f728a1fc12cab712b59c88ad24c
linxiaopeng <17352714151@163.com>
2022-03-25 22:29:25 +0800

    hot day20 最小路径和

---
+++ src/hot100/L64_最小路径和.java
@@ -0,0 +1,21 @@
+package hot100;
+
+public class L64_最小路径和 {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                } else if (i == 0) {
+                    grid[i][j] += grid[0][j-1];
+                } else if (j == 0) {
+                    grid[i][j] += grid[i-1][0];
+                } else {
+                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                }
+            }
+        }
+        return grid[m-1][n-1];
+    }
+}
---
+++ src/hot100/rre/L56_合并区间.java
@@ -0,0 +1,25 @@
+package hot100.rre;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class L56_合并区间 {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int leftPre = intervals[0][0], rightPre = intervals[0][1];
+        List<int[]> res = new ArrayList<>();
+        for (int i = 1; i < intervals.length; i++) {
+            int left = intervals[i][0], right = intervals[i][1];
+            if (rightPre >= left) {
+                rightPre = Math.max(rightPre, right);
+            } else {
+                res.add(new int[]{leftPre, rightPre});
+                leftPre = left;
+                rightPre = right;
+            }
+        }
+        res.add(new int[]{leftPre, rightPre});
+        return res.toArray(new int[0][]);
+    }
+}
